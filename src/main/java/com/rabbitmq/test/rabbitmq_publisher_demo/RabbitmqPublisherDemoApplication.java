8968bfd9e2e2ca7bb9e25f1b5cd16419ead07635
randika perera <randika9634@gmail.com>
2021-09-18 22:07:02 +0530

    frist commit

---
+++ src/main/java/com/rabbitmq/test/rabbitmq_publisher_demo/RabbitmqPublisherDemoApplication.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.test.rabbitmq_publisher_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqPublisherDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqPublisherDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rabbitmq/test/rabbitmq_publisher_demo/RabbitmqPublisherDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.test.rabbitmq_publisher_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqPublisherDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
